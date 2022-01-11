65e72b1f5ae9d50d64f2602d3336918d2f48a0d7
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-12 00:09:48 +0300

    [wip]: edit footer and header

--- app/frontend/src/footer.js
+++ app/frontend/src/footer.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap//Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 class Footer extends React.Component {
     render() {
         return (<Navbar bg="dark" variant="dark" expand="lg" fixed="bottom" className="justify-content-center">
--- app/frontend/src/header.js
+++ app/frontend/src/header.js
@@ -1,26 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap//Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import React from 'react'
 
 class Header extends React.Component {
     render() {
         return (<Navbar bg="dark" variant="dark" expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Интернет-магазин</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="/">На главную</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>

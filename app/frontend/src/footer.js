18af513d3313b36b45c96d54fb788a2cdd71a5e1
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-11 23:52:07 +0300

    [wip]: fix footer

--- app/frontend/src/footer.js
+++ app/frontend/src/footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap//Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 class Footer extends React.Component {
     render() {
-        return (<div>Footer</div>)
+        return (<Navbar bg="dark" variant="dark" expand="lg" fixed="bottom" className="justify-content-center">
+            <Navbar.Text >
+                Copyright &copy; Кокорев Я.С. 1045М
+            </Navbar.Text>
+        </Navbar>)
     }
 }
 

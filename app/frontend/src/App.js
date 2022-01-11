dd480fcb87e9b2c936daf3d74732461f19a228fd
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-11 23:16:33 +0300

    [wip]: add footer component

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
 import Header from './header'
-
+import Footer from './footer'
 
 class App extends React.Component {
 
     render() {
         return (
-            <Header />
+            <React.Fragment>
+                <Header />
+                <Footer />
+            </React.Fragment>
         )
     }
 }

5e20ba362ca519a7b5b7ff39ce9aedd7a8311492
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-19 11:26:47 +0000

    [feature]: add deploy script

---
+++ app/frontend/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Cleaning all directories"
+if [[ -d build ]]
+then
+    rm build -rf
+fi
+
+if [[ -d ../backend/public ]]
+then
+    rm ../backend/public/* -rf
+else
+    mkdir ../backend/public
+fi
+
+echo "Build React frontend"
+npm run build
+
+echo "Copied build frontend to backend"
+cp build/* ../backend/public/ -r
+
+cd ../backend
+
+echo "Running sever..."
+npm start .

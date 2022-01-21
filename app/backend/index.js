b793146b6376ac3caf7c5ad1dbd3e130f85256cd
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-21 11:52:51 +0300

    [wip]: move open db in function. Open db as READONLY for SELECR request

--- app/backend/index.js
+++ app/backend/index.js
@@ -4,7 +4,6 @@ const req = require('express/lib/request')
 const sqlite3 = require('sqlite3')
 
 const app = express()
-const db = new sqlite3.Database('db.sqlite')
 // Обработка статических файлов
 app.use(express.static(__dirname + "/public"))
 
@@ -20,6 +19,7 @@ app.get('/*', (request, response) => {
 })
 
 function get_data(send_response) {
+    let db = new sqlite3.Database('db.sqlite', sqlite3.OPEN_READONLY)
     db.all('SELECT id,name,desc,cost,img_src FROM products;', [], (err, rows) => {
         if (err) {
             throw err
@@ -28,4 +28,5 @@ function get_data(send_response) {
         let resp = JSON.stringify(rows)
         send_response(resp)
     })
+    db.close()
 }

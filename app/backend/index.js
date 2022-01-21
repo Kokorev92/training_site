c4da2806f5142e4acdd4379b6f8b868dc714b724
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-21 12:04:13 +0300

    [wip]: add server_settings.json with port. And add this file in gitignore

--- app/backend/index.js
+++ app/backend/index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const req = require('express/lib/request')
 const sqlite3 = require('sqlite3')
 
+const server_settings = require('./server_settings.json')
+
 const app = express()
 // Обработка статических файлов
 app.use(express.static(__dirname + "/public"))
 
 // Запускаем сервер
-app.listen(8000)
+app.listen(server_settings.server_port)
 
 app.get('/products', (request, response) => {
     get_data((json) => { response.send(json) })

30fc49ee9ae9fcb2673ee2df4c5efdfc771bacc4
Kokorev92 <kokorev.yaroslav@gmail.com>
2022-01-31 10:22:02 +0300

    [feature]: create default server settings file

--- app/frontend/deploy.sh
+++ app/frontend/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Cleaning all directories"
+echo "Deploy stage: cleaning all directories"
 if [[ -d build ]]
 then
     rm build -rf
@@ -13,14 +13,18 @@ else
     mkdir ../backend/public
 fi
 
-echo "Build React frontend"
+echo "Deploy stage: build React frontend"
 npm run build
 
-echo "Copying fronted to server"
+echo "Deploy stage: copying fronted to server"
 cp build/* ../backend/public/ -r
 rm ../backend/public/products
 
 cd ../backend
+
+echo "Deploy stage: configure server"
+
+echo "{\"server_port\":8000}" > server_settings.json
 
 echo "Running sever..."
 npm start .
